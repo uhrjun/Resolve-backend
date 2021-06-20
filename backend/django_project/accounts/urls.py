9931ebb8f55007973411a5899315954d1e011fd4
Arjun Choudhary <arjun99c@gmail.com>
2021-06-20 11:44:30 +0530

    changed folder names

--- backend/django_project/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
-from .views import UserRegister
-
-urlpatterns = [
-    path("", include("dj_rest_auth.urls")),
-    path("register/", UserRegister.as_view()),
-]
